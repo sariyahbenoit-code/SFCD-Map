312916c288c0a9ac25cc8d69af8002d31cccd41f
sariyahbenoit-code <sariyah_benoit@berkeley.edu>
2025-12-10 20:27:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-import mapboxgl from "https://api.mapbox.com/mapbox-gl-js/v3.17.0/mapbox-gl.js";
+//import mapboxgl from "https://api.mapbox.com/mapbox-gl-js/v3.17.0/mapbox-gl.js";
 
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.159/build/three.module.js";
+//import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.159/build/three.module.js";
 
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.159/examples/jsm/loaders/GLTFLoader.js";
+//import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.159/examples/jsm/loaders/GLTFLoader.js";
 
-import { DRACOLoader } from "https://cdn.jsdelivr.net/npm/three@0.159/examples/jsm/loaders/DRACOLoader.js";
+//import { DRACOLoader } from "https://cdn.jsdelivr.net/npm/three@0.159/examples/jsm/loaders/DRACOLoader.js";
 
 // map set up
 
